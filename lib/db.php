c10f8f3568635a74bec566eb6102a797746d7b40
Lucas1313136 <lucasprates2a@gmail.com>
2026-09-07 22:55:22 -0300

    garantia de criacao da pasta do banco de dados

--- lib/db.php
+++ lib/db.php
@@ -1,6 +1,11 @@
 <?php
     require_once __DIR__ . '/rb.php';
 
+    $pastaBanco = __DIR__ . '/../db';
+    if (!is_dir($pastaBanco)) {
+        mkdir($pastaBanco, 0777, true);
+    }
+
     $caminhoBanco = __DIR__ . '/../db/db.sqlite';
 
     R::setup("sqlite:" . $caminhoBanco);
